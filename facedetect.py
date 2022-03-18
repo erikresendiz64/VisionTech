26d57bc762f9a3c48c367c1ca68518bad1224b57
eresen3 <59971386+eresen3@users.noreply.github.com>
2022-03-18 12:16:36 -0500

    addface

--- facedetect.py
+++ facedetect.py
@@ -1,3 +1,6 @@
+import os
+import glob
+import re
 from os import mkdir
 import cv2
 import time
@@ -8,18 +11,34 @@ name = input('Enter Name: ')
 FD = FDmodule.FaceDetector(0.75)
 cam = cv2.VideoCapture(0)
 pTime = 0
+if not os.path.exists(f'./Data/{name}'):
+    mkdir(f"./Data/{name}")
+    numImgs = 0
+else:
+    dir = os.listdir(f'./Data/{name}')
+    if len(dir) == 0:
+        numImgs = 0
+    else:
+        list_of_files = glob.glob(f'./Data/{name}/*jpg') # * means all if need specific format then *.csv
+        latest_file = max(list_of_files, key=os.path.getctime)
+        parsed = latest_file.split('/')
+        numLoc = parsed[3]
+        imgNum = re.findall('[0-9]+', numLoc)
+        imgs = int(imgNum[0])
+
+        numImgs = int(imgNum[0]) * 50
+
 
 print("\n[INFO] Stand in the camera's view")
-numImgs = 0
 while True:
     ret, frame = cam.read() #read each frame, return true if a frame exists
     frame, bounds = FD.findFaces(frame)
     numImgs += 1
-    cv2.imshow("Running", frame)
     if numImgs % 50 == 0:
-        cv2.imwrite("Data/" + str(name) + str(int(numImgs/50)) + ".jpg", frame)
+        cv2.imwrite(f"./Data/{name}/{name}{int(numImgs/50)}.jpg", frame)
+    cv2.imshow("Running", frame)
     k = cv2.waitKey(1)
     if k % 256 == 27:
         break
-    elif numImgs >= 100: # Take 30 face sample and stop video
+    elif numImgs >= 200: # Take 30 face sample and stop video
         break
